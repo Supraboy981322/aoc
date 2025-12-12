66ac06f5adf65c9ca313cf4f1c3afc97b8b9a020
don't <97927259+Supraboy981322@users.noreply.github.com>
2025-12-11 22:11:38 -0600

    start doing past years

---
+++ 2023/README.md
@@ -0,0 +1,3 @@
+# Why I am doing past years 
+
+I am doing past years.... well just because. Not sure why. Just felt like it.
---
+++ 2023/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var (
+	maxC = map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+)
+
+func main() {
+	inR, _ := os.ReadFile("in2.txt")
+	games := strings.Split(string(inR), "\n")
+	games = games[:len(games)-1]
+	var res int
+	for i, gameR := range games {
+		game := strings.Split(gameR, ":")[1]
+		sets := strings.Split(game, ";")
+		valid := true
+		for _, set := range sets {
+			for _, cubeR := range strings.Split(set, ",") {
+				cube := cubeR[1:]
+				col := strings.Split(cube, " ")[1]
+				val, _ := strconv.Atoi(strings.Split(cube, " ")[0])
+				if maxC[col] < val {
+					valid = false
+				}
+			}
+		}
+		if valid { res += i+1 }
+	}
+	fmt.Printf("%d\n", res)
+}
